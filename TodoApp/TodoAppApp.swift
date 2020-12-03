c38163981293ddf48e6131b622a9864f2ffe514c
Timur Zadvornov <weazy.exe@gmail.com>
2020-12-03 15:41:29 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Timur Zadvornov on 03.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
